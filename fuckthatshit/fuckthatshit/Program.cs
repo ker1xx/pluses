2539f3625c56a24043b316264bf6416b1c03ffd0
ker1xx <klinkamama@gmail.com>
2023-04-24 16:32:34 +0300

    10 done

---
+++ fuckthatshit/fuckthatshit/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace fuckthatshit
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            char a = 'a';
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine((a.ToString()).GetHashCode());
+        }
+    }
+}
