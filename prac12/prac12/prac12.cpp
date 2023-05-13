332ad1484c4495fcc7b35befc8655eb45907c729
ker1xx <112649840+ker1xx@users.noreply.github.com>
2023-05-13 22:56:35 +0300

    11&12

---
+++ prac12/prac12/prac12.cpp
@@ -0,0 +1,83 @@
+﻿#include <Windows.h>
+#include <string>
+#include <vector>
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+class Animal {
+protected:
+    string Name;
+public:
+    Animal(string Name) {
+        this->Name = Name;
+    }
+    Animal() {};
+};
+class Pet  {
+protected:
+      int NeedEat, GiveEat;
+public:
+    Pet(  int NeedEat,   int GiveEat) {
+        this->NeedEat = NeedEat;
+        this->GiveEat = GiveEat;
+    };
+    virtual void feed() {};
+
+};
+class Cat : public Animal, Pet {
+public:
+
+    Cat(string Name,   int NeedEat,   int GiveEat): Animal(Name), Pet(NeedEat, GiveEat) {
+        this->Name = Name;
+        this->NeedEat = NeedEat;
+        this->GiveEat = GiveEat;
+    }
+    void feed(string& Name,   int &NeedEat,   int &GiveEat) {
+        if (this->GiveEat + 10 < GiveEat || this->GiveEat - 10 > NeedEat)
+            cout << "котик сдох(" << endl;
+        else
+            cout << "УРА КОТ ВЫЖИЛ!!" << endl;
+    }
+};
+class Dog : public Animal, Pet {
+public:
+    Dog(string Name,   int NeedEat,   int GiveEat) : Animal(Name), Pet(NeedEat, GiveEat) {
+        this->Name = Name;
+        this->NeedEat = NeedEat;
+        this->GiveEat = GiveEat;
+    }
+    void feed(string& Name,   int& NeedEat,   int& GiveEat) {
+        if (this->GiveEat + 10 < GiveEat || this->GiveEat - 10 > NeedEat)
+            cout << "собачка сдохла(" << endl;
+        else
+            cout << "УРА САБАКА ВЫЖИЛА!!" << endl;
+    }
+};
+
+
+int main()
+{
+    setlocale(0, "ru");
+    SetConsoleCP(1251);
+    srand(time(0));
+      int random1 = 1 + rand() % 101;
+      int random2 = 1 + rand() % 101;
+    string NameOfCat;
+    string NameOfDog;
+      int InputEatForCat, InputEatForDog;
+    cout << "Введите имя кота" << endl;
+    cin >> NameOfCat;    
+    cout << "Введите количество корма для кота" << endl;
+    cin >> InputEatForCat;
+    cout << "Введите имя собаки" << endl;
+    cin >> NameOfDog;    
+    cout << "Введите количество корма для собака" << endl;
+    cin >> InputEatForDog;
+    Cat newCat(NameOfCat, random1, InputEatForCat);
+    Dog NewDog(NameOfDog, random2, InputEatForDog);
+    newCat.feed(NameOfCat, random1, InputEatForCat);
+    NewDog.feed(NameOfCat, random1, InputEatForCat);
+}
+
---
+++ pracfile/pracfile/pracfile.cpp
@@ -0,0 +1,62 @@
+﻿#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <iostream>
+using namespace std;
+
+template <class T> 
+class Serialization {
+public:
+void    Serializtion() {};
+    void save(string file_name, T object);
+    void load(string file_name, T& object);
+};
+template <class T> 
+void Serialization<T>::save(string fileNmae, T object) {
+    object.write(fileNmae);
+}
+template<class T>
+void Serialization<T>::load(string fileNmae, T& object) {
+    object.read(fileNmae);
+}
+class Person {
+public:
+    string name;
+    int age;
+    Person(string name, int age);
+    Person() {};
+    void write(string fileNmae);
+    void read(string fileNmae);
+};
+Person::Person(string name, int age) {
+    this->name = name;
+    this->age = age;
+}
+void Person::write(string fileNmae) {
+std:ofstream out(fileNmae);
+    out << name << " " << age << endl;
+    out.close();
+}
+void Person::read(string fileNmae) {
+    ifstream in(fileNmae);
+    if (in.is_open()) {
+        getline(in, name);
+        in >> age;
+    }
+    in.close();
+}
+
+void main()
+{
+    setlocale(0, "ru");
+    SetConsoleCP(0);
+    Person p("sanya", 25);
+    Serialization<Person> s;
+    s.save("file.txt", p);
+
+    Person new_p("",0);
+    s.load("file.txt", new_p);
+    new_p.age = stoi(new_p.name.substr(new_p.name.find_last_of(" ")));
+    new_p.name = new_p.name.substr(0, new_p.name.find_last_of(" "));
+    cout << new_p.name << " - " << new_p.age << endl;
+}
