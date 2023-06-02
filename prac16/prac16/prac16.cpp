0c7eb2355cb57ad69c76d33b151d4411548768c6
ker1xx <klinkamama@gmail.com>
2023-06-02 23:43:59 +0300

    fixdllandcontainers

---
+++ prac16/prac16/prac16.cpp
@@ -0,0 +1,85 @@
+﻿#include <memory>
+#include <Windows.h>
+#include <string>
+#include <iostream>
+#include <regex>
+#include <memory>
+
+using namespace std;
+
+class PersonException
+{
+public:
+	PersonException(string message) : message{ message } {}
+	string getMessage() const {
+		return message;
+	}
+private:
+	string message;
+};
+
+class Person {
+private:
+	string name;
+	unsigned short age;
+	unsigned short id;
+public:
+	Person(string name, unsigned short age, unsigned short id) {
+		try {
+			if (age > 100)
+			{
+				throw PersonException{ "Invalid age" };
+			}
+			if (!regex_match(name, regex("^[А-Яа-я]+$")))
+			{
+				throw PersonException{ "Wrong name" };
+			}
+			this->name = name;
+			this->age = age;
+			this->id = id;
+		}
+		catch (const PersonException error) {
+			cout << error.getMessage() << endl;
+		}
+	}
+	string getname() {
+		return name;
+	}
+	unsigned short getage() {
+		return age;
+	}
+	unsigned short getid() {
+		return id;
+	}
+};
+shared_ptr <Person> func(string name, unsigned short age, unsigned id)
+{
+	return shared_ptr <Person>(new Person(name, age, id));
+}
+shared_ptr <Person> print(shared_ptr <Person> person) {
+	cout << "Имя: " << person->getname() << " Возраст: " << person->getage() << " ID: " << person->getid();
+	return person;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	string name;
+	unsigned short age, id;
+	cout << "введите имя: " << endl;
+	cin >> name;
+	cout << "введите возраст: " << endl;
+	cin >> age;
+	cout << "введите id: " << endl;
+	cin >> id;
+	try
+	{
+		shared_ptr <Person> newperson(func(name, age, id));
+		shared_ptr <Person> newperson2 = print(newperson);
+	}
+	catch (const char* error)
+	{
+		cout << "Ошибка: " << error << endl;
+	}
+}
---
+++ prac17/prac17/prac17.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <string>
+#include <list>
+#include <forward_list>
+#include <queue>
+#include <stack>
+#include <array>
+#include <Windows.h>
+
+using namespace std;
+
+struct Book {
+private:
+	string name;
+	string author;
+	string genre;
+	int year;
+public:
+	Book(string name, string author, string genre, int year)
+	{
+		this->name = name;
+		this->genre = genre;
+		this->author = author;
+		this->year = year;
+	}
+	string getname()
+	{
+		return name;
+	}
+	string getauthor() {
+		return author;
+	}
+	string getgenre() {
+		return genre;
+	}
+	int getyear() {
+		return year;
+	}
+};
+
+void showbook(Book book) {
+	cout << "название: " << book.getname() << "\tавтор: " << book.getauthor() << "\tжанр: " << book.getgenre() << "\tгод: " << book.getyear() << endl;
+}
+Book createbook() {
+	string name, author, genre;
+	int year;
+	cout << "введите название: " << endl;
+	cin >> name;
+	cout << "введите автора: " << endl;
+	cin >> author;
+	cout << "введите жанр: " << endl;
+	cin >> genre;
+	cout << "введите год издания: " << endl;
+	cin >> year;
+	Book book{ name,author,genre,year };
+	return book;
+}
+bool comparingyear(Book book1, Book book2)
+{
+	if (book1.getyear() < book2.getyear())
+		return true;
+	else
+		return false;
+}
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	array<Book, 5> ar{ Book{"","","",0},Book{"","","",0},Book{"","","",0},Book{"","","",0},Book{"","","",0} };
+	vector<Book> vct{};
+	list<Book> lt;
+	forward_list<Book> flt;
+	for (int i = 0; i < 5; i++)
+	{
+		Book newbook = createbook();
+		ar[i] = newbook;
+		vct.push_back(newbook);
+		lt.push_back(newbook);
+		flt.push_front(newbook);
+	}
+	cout << "это стек:" << endl;
+	for (int i = vct.size()-1; i >= 0; i--)
+	{
+		
+		showbook(vct[i]);
+	}
+	list<Book> temp = lt;
+	cout << "это куеуе: " << endl;
+	for (int i = 0; i < lt.size(); i++)
+	{
+		showbook(temp.front());
+		temp.pop_front();
+	}
+	priority_queue <int> year;
+	cout << " это приорити куеуе: " << endl;
+	for (int i = 0; i < ar.size(); i++)
+	{
+		year.push(ar[i].getyear());
+	}
+	priority_queue <int> year1; 
+	year1.swap(year);
+	for (int i = year1.size() - 1; i >= 0; i--)
+	{
+		for (int j = 0; j < vct.size(); j++)
+		{
+			if (year1.top() == vct[j].getyear())
+			{
+				showbook(vct[j]);
+				year1.pop();
+				break;
+			}
+		}
+	}
+}
